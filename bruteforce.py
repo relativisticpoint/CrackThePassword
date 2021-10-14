ab3f1f6774fabdfda4257516f467fe235caeea96
relativisticpoint <73722350+relativisticpoint@users.noreply.github.com>
2021-10-14 18:06:19 +0200

    Add files via upload

--- bruteforce.py
+++ bruteforce.py
@@ -1,5 +1,8 @@
+"""
+ @RaphaelVallat a tt fait moi j ai juste bidouille
+"""
+import numpy
 import string
-import nympy
 from itertools import product
 from time import time
 from numpy import loadtxt
@@ -15,19 +18,21 @@ def product_loop(password, generator):
 
 def bruteforce(password, max_nchar=8):
     """Password brute-force algorithm.
+
     Parameters
     ----------
     password : string
         To-be-found password.
     max_nchar : int
         Maximum number of characters of password.
+
     Return
     ------
     bruteforce_password : string
         Brute-forced password
     """
     print('1) Comparing with most common passwords / first names')
-    common_pass = nympy.loadtxt('probable-v2-top12000.txt', dtype=str)
+    common_pass = loadtxt('probable-v2-top12000.txt', dtype=str)
     common_names = loadtxt('middle-names.txt', dtype=str)
     cp = [c for c in common_pass if c == password]
     cn = [c for c in common_names if c == password]
@@ -42,7 +47,8 @@ def bruteforce(password, max_nchar=8):
     if len(cnl) == 1:
         print('\nPassword:', cnl)
         return cnl
-
+    """
+    # this find the 
     print('2) Digits cartesian product')
     for l in range(1, 9):
         generator = product(string.digits, repeat=int(l))
@@ -50,6 +56,27 @@ def bruteforce(password, max_nchar=8):
         p = product_loop(password, generator)
         if p is not False:
             return p
+    """
+    # i ll try to implement this method
+
+    print('si on connait le nombre de char')
+    generator = product(string.ascii_lowercase,repeat=int(max_nchar))
+    p = product_loop(password,generator)
+    if p is not False:
+        return p
+
+    print(' 2bis) ASCII lowercase')
+    # relativisticpoint:the pb with this method is that it tries to find the pswd using all the 
+    # combinations i think we could significantly reduce the time by precising 
+    # the number of characters we re gonna use
+    for l in range(1, max_nchar + 1):
+        print("\t..%d char" % l)
+        generator = product(string.ascii_lowercase,
+                            repeat=int(l))
+        p = product_loop(password, generator)
+        if p is not False:
+            return p
+    
 
     print('3) Digits + ASCII lowercase')
     for l in range(1, max_nchar + 1):
@@ -59,6 +86,7 @@ def bruteforce(password, max_nchar=8):
         p = product_loop(password, generator)
         if p is not False:
             return p
+    
 
     print('4) Digits + ASCII lower / upper + punctuation')
     # If it fails, we start brute-forcing the 'hard' way
@@ -74,7 +102,9 @@ def bruteforce(password, max_nchar=8):
 
 
 # EXAMPLE
+
+# passw = str(input('type in the password you want to bruteforce : '))
 start = time()
-bruteforce('sunshine') # Try with '123456' or '751345' or 'test2018'
+bruteforce('leopol',6) # changed the method now you have to specify the number of char that are contained in the passwrd pour aller plus vite
 end = time()
 print('Total time: %.2f seconds' % (end - start))
