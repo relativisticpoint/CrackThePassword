944a184909d3af5e45508b7c853e00a123110745
relativisticpoint <hsami272@gmail.com>
2022-02-04 00:01:12 +0100

    working

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CrackThePassword
-This is a fun python/golang program to simulate a password cracking by bruteforce
+This is a fun python/golang multi-threaded program to simulate a password cracking by bruteforce
+# The Python code :
+to run the python code open a terminal on the folder and execute the folloxing command 
+```
+python2 bruteforce.py <the_password_you_want_to_crack>
+```
+We use a python2 interpreter because it runs exception-free, the script works with python3 as well but calls some annoyhing exceptions from the multithreading module (this is probably due to a change of support from python2 to python3 that hasn't been updated on the multithreading library..)
+
+The program will first look into a list of common passwords and a list of common names to try and find your password, and at the same time, it bruteforces it by testing different combinations of letters and checking wether they match with your password, whichever method finds your password first will return it.
+Note That: in this first version of the program, the password you pass as an argument must be lowercase and must not contain numbers nor special caracters, this will come with the following versions
+The main aim of this mini-project is to get an understanding of how complex bruteforce may get when you start dealing with longer passwords, you can try out several combinations, and you'll remark upon the fact that the longer your password gets the more time it takes for the program to crack, untill you reach a certain lenght that would make it basically impossible to bruteforce (technically it's still possible but you would have to let the program run for days/weeks/years )
+Have Fun !
+
--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,5 @@
-"""
- 
-"""
+
+from threading import Thread
 import numpy
 import string
 from itertools import product
@@ -8,7 +7,8 @@ from time import time
 from numpy import loadtxt
 from multiprocessing import Process
 import multiprocessing as mp
-
+import threading
+import sys
 
 def product_loop(password, generator):
     for p in generator:
@@ -18,7 +18,7 @@ def product_loop(password, generator):
     return False
 
 def cherche(password,q):
-    print("1ere etape : comparaison avec les mdp les plus communs")
+    print("comparaison avec les mdp les plus communs")
     common_pass = loadtxt('probable-v2-top12000.txt', dtype=str)
     common_names = loadtxt('middle-names.txt', dtype=str)
     cp = [c for c in common_pass if c == password]
@@ -34,114 +34,39 @@ def cherche(password,q):
     if len(cnl) == 1:
         print('\nPassword:', cnl)
         q.put(cnl)
-    q.put('end')
+    else:
+        q_check.put('fail1')
 
 def cherche2(password,q,max_char):
-    if q.get()== 'end':
-        print("le process de recherche 2 est lance")
-        generator = product(string.ascii_lowercase,repeat=int(max_char))
-        p = product_loop(password,generator)
-        if p is not False:
-            q.put(p)
-
-    
-    
-    
-        
-    
-
-def bruteforce(password, max_nchar=8):
-    """Password brute-force algorithm.
-
-    Parameters
-    ----------
-    password : string
-        To-be-found password.
-    max_nchar : int
-        Maximum number of characters of password.
-
-    Return
-    ------
-    bruteforce_password : string
-        Brute-forced password
-    """
-    
-    """
-    # this find the 
-    print('2) Digits cartesian product')
-    for l in range(1, 9):
-        generator = product(string.digits, repeat=int(l))
-        print("\t..%d digit" % l)
-        p = product_loop(password, generator)
-        if p is not False:
-            return p
-    """
-    # i ll try to implement this method
-
-    print('si on connait le nombre de char')
-    generator = product(string.ascii_lowercase,repeat=int(max_nchar))
+    print("recherche aleatoire")
+    generator = product(string.ascii_lowercase,repeat=int(max_char))
     p = product_loop(password,generator)
+    print(p)
     if p is not False:
-        return p
+        print('\nPassword:', p)
+        q.put(p)
 
-    print(' 2bis) ASCII lowercase')
-   
-    for l in range(1, max_nchar + 1):
-        print("\t..%d char" % l)
-        generator = product(string.ascii_lowercase,
-                            repeat=int(l))
-        p = product_loop(password, generator)
-        if p is not False:
-            return p
-    
-
-    print('3) Digits + ASCII lowercase')
-    for l in range(1, max_nchar + 1):
-        print("\t..%d char" % l)
-        generator = product(string.digits + string.ascii_lowercase,
-                            repeat=int(l))
-        p = product_loop(password, generator)
-        if p is not False:
-            return p
-    
-
-    print('4) Digits + ASCII lower / upper + punctuation')
-    # If it fails, we start brute-forcing the 'hard' way
-    # Same as possible_char = string.printable[:-5]
-    all_char = string.digits + string.ascii_letters + string.punctuation
-
-    for l in range(1, max_nchar + 1):
-        print("\t..%d char" % l)
-        generator = product(all_char, repeat=int(l))
-        p = product_loop(password, generator)
-        if p is not False:
-            return p
-
-
-# EXAMPLE
-
-# # passw = str(input('type in the password you want to bruteforce : '))
-# start = time()
-# bruteforce('leopol',6) # changed the method now you have to specify the number of char that are contained in the passwrd pour aller plus vite
-# end = time()
-# print('Total time: %.2f seconds' % (end - start))
 if __name__ == '__main__':
-    password = 'dkj'
-    max_char=3
+
+    q_check = mp.Queue()
     q = mp.Queue()
-    q.put('end')
+    password = sys.argv[1]
+    p=threading.Thread(target=cherche,args=(password,q,))
+    p1=threading.Thread(target=cherche2,args=(password,q,len(password),))
     start = time()
-    # p= Process(target=bruteforce,args=(password,max_char))
-    # p.start()
-    # p.join()
-    p=Process(target=cherche,args=(password,q))
-    p1=Process(target=cherche2,args=(password,q,max_char))
     p.start()
     p1.start()
     p.join()
+    end1 = time()
     p1.join()
-    print("mdp dans ce qui suit",q.get())
     end = time()
-    print('Total time: %.2f seconds' % (end - start))
+    if q.get() != None:
+        print("I found your password in {} seconds".format(end1-start))
+    else:
+        print("we couldn't find the password, maybe it s too long...")
 
+
+
+
+    
     
