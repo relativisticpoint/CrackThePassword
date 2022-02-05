55a6077be996075e07688a875012b82ad64f1f49
N03M0TT3 <noemotte@gmail.com>
2022-02-05 15:31:49 +0100

    File reader applied to the password.txt file

--- main.go
+++ main.go
@@ -1,33 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"sync"
-	"time"
+	"os"
 )
 
-var password int
-var wg sync.WaitGroup
+// var password int
+// var wg sync.WaitGroup
+
+// func main() {
+// 	password = 9999999999
+// 	fmt.Printf("#DEBUT DU CODE\n")
+// 	start := time.Now()
+
+// 	for i := 0; i < 10; i++ {
+// 		go testInterval(10000000000*i, 10000000000*(i+1))
+// 	}
+
+// 	elapsed := time.Now().Sub(start)
+// 	fmt.Printf("Temps écoulé : %v\n", elapsed)
+// 	fmt.Printf("#FIN DU CODE\n")
+// }
+
+// func testInterval(from int, to int) {
+// 	for i := from; i <= to; i++ {
+// 		if i == password {
+// 			fmt.Printf("Code trouvé ! C'était : %d\n", password)
+// 			break
+// 		}
+// 	}
+// }
 
 func main() {
-	password = 9999999999
-	fmt.Printf("#DEBUT DU CODE\n")
-	start := time.Now()
+	f, err := os.Open("./password.txt")
+	handleErr(err)
 
-	for i := 0; i < 10; i++ {
-		go testInterval(10000000000*i, 10000000000*(i+1))
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	password := ""
+
+	for scanner.Scan() {
+		password = scanner.Text()
 	}
 
-	elapsed := time.Now().Sub(start)
-	fmt.Printf("Temps écoulé : %v\n", elapsed)
-	fmt.Printf("#FIN DU CODE\n")
+	fmt.Println(password)
+
 }
 
-func testInterval(from int, to int) {
-	for i := from; i <= to; i++ {
-		if i == password {
-			fmt.Printf("Code trouvé ! C'était : %d\n", password)
-			break
-		}
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("Une erreur est survenu : ")
+		fmt.Println(err)
 	}
 }
