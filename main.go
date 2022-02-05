3ee3891e182249badde04c886b747dca096a2bf1
N03M0TT3 <noemotte@gmail.com>
2022-02-05 15:33:46 +0100

    Just cleaning

--- main.go
+++ main.go
@@ -6,32 +6,6 @@ import (
 	"os"
 )
 
-// var password int
-// var wg sync.WaitGroup
-
-// func main() {
-// 	password = 9999999999
-// 	fmt.Printf("#DEBUT DU CODE\n")
-// 	start := time.Now()
-
-// 	for i := 0; i < 10; i++ {
-// 		go testInterval(10000000000*i, 10000000000*(i+1))
-// 	}
-
-// 	elapsed := time.Now().Sub(start)
-// 	fmt.Printf("Temps écoulé : %v\n", elapsed)
-// 	fmt.Printf("#FIN DU CODE\n")
-// }
-
-// func testInterval(from int, to int) {
-// 	for i := from; i <= to; i++ {
-// 		if i == password {
-// 			fmt.Printf("Code trouvé ! C'était : %d\n", password)
-// 			break
-// 		}
-// 	}
-// }
-
 func main() {
 	f, err := os.Open("./password.txt")
 	handleErr(err)
